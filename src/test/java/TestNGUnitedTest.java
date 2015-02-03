e435e45b9b0fd0874acfa41f879583b7f55df0c0
Uzi Eilon <PerfectoMobile256@gmail.com>
2015-02-03 10:27:40 -0500

    Update TestNGUnitedTest.java

--- src/test/java/TestNGUnitedTest.java
+++ src/test/java/TestNGUnitedTest.java
@@ -85,7 +85,7 @@ public class TestNGUnitedTest {
 	 @DataProvider(name = "Devices" , parallel = true)
 	    public Object[][] testSumInput() {
 	        return new Object[][] { { "Android", "0149BCA71700D01F" }, 
-	        			{ "Android", "A1A5438E" } ,
+	        			{ "Android", "57AF4ED9" } ,
 	        			{"Iphone","71EDDAE8C07850B0A910F2CF9E69A93095B996DA"},
 	        			{"iphone","39F3DA5531ADBE2A05CFF4D65E43A2C38D3D595A"}
 	        };
